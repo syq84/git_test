c75bf65c03df78b5b1069f224715bfc33e389fb3
syq <sm08401@163.com>
2022-10-16 13:19:33 +0800

    first commit

---
+++ src/main/java/com/syq/GitTest.java
@@ -0,0 +1,11 @@
+package com.syq;
+
+/**
+ * @author syq
+ * @create 2022-10-16 13:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
